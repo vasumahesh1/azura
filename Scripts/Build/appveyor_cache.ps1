53c7da7f2706a099c6fec3b402ed0cf8c1efdb7d
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-08-11 21:30:31 -0400

    add: boost

--- Scripts/Build/appveyor_cache.ps1
+++ Scripts/Build/appveyor_cache.ps1
@@ -1,8 +1,9 @@
 $testRoot = Test-Path -Path C:\AppveyorCache\
 $testNinja = Test-Path -Path C:\AppveyorCache\Ninja\
 $testVulkan = Test-Path -Path C:\AppveyorCache\VulkanSDK\
+$testBoost = Test-Path -Path C:\AppveyorCache\Boost\
 
-if($testRoot -and $testNinja -and $testVulkan) {
+if($testRoot -and $testNinja -and $testVulkan -and $testBoost) {
     echo "Cache already found ..."
     exit 0
 }
@@ -17,6 +18,16 @@ echo "Downloading Ninja ..."
 appveyor DownloadFile $env:NINJA_URL -FileName ninja.zip > $null
 7z x ninja.zip -oC:\AppveyorCache\Ninja\ > $null
 
+# Download Boost and Build it
+echo "Downloading Boost ..."
+appveyor DownloadFile $env:BOOST_URL -FileName boost.zip > $null
+7z x boost.zip -oC:\AppveyorCache\Boost\ > $null
+cd C:\AppveyorCache\Boost\boost_1_68_0\
+
+echo "Building Boost ..."
+cmd /c ".\boostrap && exit"
+cmd /c ".\b2 && exit"
+
 # Download and setup Vulkan
 echo "Downloading VulkanSDK ..."
 cd ..
@@ -25,6 +36,8 @@ appveyor DownloadFile $env:VULKAN_URL -FileName Vulkan.exe > $null
 echo "Installing VulkanSDK ..."
 cmd /c "Vulkan.exe /S && exit" > $null
 Copy-Item -Path "C:\VulkanSDK\*" -Destination "C:\AppveyorCache\VulkanSDK\" -Recurse -Force > $null
+
+
 
 echo "Dependencies Downloaded."
 exit 0
