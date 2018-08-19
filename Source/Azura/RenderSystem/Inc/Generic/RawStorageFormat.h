8a3519e941788a3e05165e3ae5be5a578fdbb35b
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-08-19 12:08:07 -0400

    add: improve vkformat mapping

--- Source/Azura/RenderSystem/Inc/Generic/RawStorageFormat.h
+++ Source/Azura/RenderSystem/Inc/Generic/RawStorageFormat.h
@@ -60,9 +60,68 @@ using ChannelDesc = std::tuple<Channel, ChannelDataType, U32>;
 #define CHANNEL_TUPLE_SIZE 3
 
 // Format Iterator
-#define RAW_STORAGE_FORMAT_ITERATOR(FUNC)                                                                                                                                   \
-  FUNC(( UNKNOWN,                   /* CHANNELS START */ ((Unknown, Typeless, 0))                                                             /* CHANNELS END */ ))         \
-  FUNC(( R32G32B32A32_FLOAT,        /* CHANNELS START */ ((Red, Float, 32)) ((Blue, Float, 32)) ((Green, Float, 32)) ((Alpha, Float, 32))     /* CHANNELS END */ ))
+#define RAW_STORAGE_FORMAT_ITERATOR(FUNC)                                                                                                                                                \
+  FUNC(( UNKNOWN,                         /* CHANNELS START */ ((Unknown, Typeless, 0))                                                                             /* CHANNELS END */)) \
+  FUNC(( R16_FLOAT,                       /* CHANNELS START */ ((Red, Float, 16))                                                                                   /* CHANNELS END */)) \
+  FUNC(( R16_UINT,                        /* CHANNELS START */ ((Red, UInt, 16))                                                                                    /* CHANNELS END */)) \
+  FUNC(( R16_SINT,                        /* CHANNELS START */ ((Red, SInt, 16))                                                                                    /* CHANNELS END */)) \
+  FUNC(( R16_UNORM,                       /* CHANNELS START */ ((Red, UNorm, 16))                                                                                   /* CHANNELS END */)) \
+  FUNC(( R16_SNORM,                       /* CHANNELS START */ ((Red, SNorm, 16))                                                                                   /* CHANNELS END */)) \
+  FUNC(( R16_USCALED,                     /* CHANNELS START */ ((Red, UScaled, 16))                                                                                 /* CHANNELS END */)) \
+  FUNC(( R16_SSCALED,                     /* CHANNELS START */ ((Red, SScaled, 16))                                                                                 /* CHANNELS END */)) \
+  FUNC(( R32_FLOAT,                       /* CHANNELS START */ ((Red, Float, 32))                                                                                   /* CHANNELS END */)) \
+  FUNC(( R32_UINT,                        /* CHANNELS START */ ((Red, UInt, 32))                                                                                    /* CHANNELS END */)) \
+  FUNC(( R32_SINT,                        /* CHANNELS START */ ((Red, SInt, 32))                                                                                    /* CHANNELS END */)) \
+  FUNC(( R32_UNORM,                       /* CHANNELS START */ ((Red, UNorm, 32))                                                                                   /* CHANNELS END */)) \
+  FUNC(( R32_SNORM,                       /* CHANNELS START */ ((Red, SNorm, 32))                                                                                   /* CHANNELS END */)) \
+  FUNC(( R16G16_FLOAT,                    /* CHANNELS START */ ((Red, Float, 16))         ((Green, Float, 16))                                                      /* CHANNELS END */)) \
+  FUNC(( R16G16_UINT,                     /* CHANNELS START */ ((Red, UInt, 16))          ((Green, UInt, 16))                                                       /* CHANNELS END */)) \
+  FUNC(( R16G16_SINT,                     /* CHANNELS START */ ((Red, SInt, 16))          ((Green, SInt, 16))                                                       /* CHANNELS END */)) \
+  FUNC(( R16G16_UNORM,                    /* CHANNELS START */ ((Red, UNorm, 16))         ((Green, UNorm, 16))                                                      /* CHANNELS END */)) \
+  FUNC(( R16G16_SNORM,                    /* CHANNELS START */ ((Red, SNorm, 16))         ((Green, SNorm, 16))                                                      /* CHANNELS END */)) \
+  FUNC(( R16G16_USCALED,                  /* CHANNELS START */ ((Red, UScaled, 16))       ((Green, UScaled, 16))                                                    /* CHANNELS END */)) \
+  FUNC(( R16G16_SSCALED,                  /* CHANNELS START */ ((Red, SScaled, 16))       ((Green, SScaled, 16))                                                    /* CHANNELS END */)) \
+  FUNC(( R11G11B10_FLOAT,                 /* CHANNELS START */ ((Red, Float, 11))         ((Green, Float, 11))    ((Blue, Float, 10))                               /* CHANNELS END */)) \
+  FUNC(( R16G16B16_FLOAT,                 /* CHANNELS START */ ((Red, Float, 16))         ((Green, Float, 16))    ((Blue, Float, 16))                               /* CHANNELS END */)) \
+  FUNC(( R16G16B16_UINT,                  /* CHANNELS START */ ((Red, UInt, 16))          ((Green, UInt, 16))     ((Blue, UInt, 16))                                /* CHANNELS END */)) \
+  FUNC(( R16G16B16_SINT,                  /* CHANNELS START */ ((Red, SInt, 16))          ((Green, SInt, 16))     ((Blue, SInt, 16))                                /* CHANNELS END */)) \
+  FUNC(( R16G16B16_UNORM,                 /* CHANNELS START */ ((Red, UNorm, 16))         ((Green, UNorm, 16))    ((Blue, UNorm, 16))                               /* CHANNELS END */)) \
+  FUNC(( R16G16B16_SNORM,                 /* CHANNELS START */ ((Red, SNorm, 16))         ((Green, SNorm, 16))    ((Blue, SNorm, 16))                               /* CHANNELS END */)) \
+  FUNC(( R16G16B16_USCALED,               /* CHANNELS START */ ((Red, UScaled, 16))       ((Green, UScaled, 16))  ((Blue, UScaled, 16))                             /* CHANNELS END */)) \
+  FUNC(( R16G16B16_SSCALED,               /* CHANNELS START */ ((Red, SScaled, 16))       ((Green, SScaled, 16))  ((Blue, SScaled, 16))                             /* CHANNELS END */)) \
+  FUNC(( R64_FLOAT,                       /* CHANNELS START */ ((Red, Float, 64))                                                                                   /* CHANNELS END */)) \
+  FUNC(( R64_UINT,                        /* CHANNELS START */ ((Red, UInt, 64))                                                                                    /* CHANNELS END */)) \
+  FUNC(( R64_SINT,                        /* CHANNELS START */ ((Red, SInt, 64))                                                                                    /* CHANNELS END */)) \
+  FUNC(( R32G32_FLOAT,                    /* CHANNELS START */ ((Red, Float, 32))         ((Green, Float, 32))                                                      /* CHANNELS END */)) \
+  FUNC(( R32G32_UINT,                     /* CHANNELS START */ ((Red, UInt, 32))          ((Green, UInt, 32))                                                       /* CHANNELS END */)) \
+  FUNC(( R32G32_SINT,                     /* CHANNELS START */ ((Red, SInt, 32))          ((Green, SInt, 32))                                                       /* CHANNELS END */)) \
+  FUNC(( R16G16B16A16_FLOAT,              /* CHANNELS START */ ((Red, Float, 16))         ((Green, Float, 16))    ((Blue, Float, 16))     ((Alpha, Float, 16))      /* CHANNELS END */)) \
+  FUNC(( R16G16B16A16_UINT,               /* CHANNELS START */ ((Red, UInt, 16))          ((Green, UInt, 16))     ((Blue, UInt, 16))      ((Alpha, UInt, 16))       /* CHANNELS END */)) \
+  FUNC(( R16G16B16A16_SINT,               /* CHANNELS START */ ((Red, SInt, 16))          ((Green, SInt, 16))     ((Blue, SInt, 16))      ((Alpha, SInt, 16))       /* CHANNELS END */)) \
+  FUNC(( R16G16B16A16_UNORM,              /* CHANNELS START */ ((Red, UNorm, 16))         ((Green, UNorm, 16))    ((Blue, UNorm, 16))     ((Alpha, UNorm, 16))      /* CHANNELS END */)) \
+  FUNC(( R16G16B16A16_SNORM,              /* CHANNELS START */ ((Red, SNorm, 16))         ((Green, SNorm, 16))    ((Blue, SNorm, 16))     ((Alpha, SNorm, 16))      /* CHANNELS END */)) \
+  FUNC(( R16G16B16A16_USCALED,            /* CHANNELS START */ ((Red, UScaled, 16))       ((Green, UScaled, 16))  ((Blue, UScaled, 16))   ((Alpha, UScaled, 16))    /* CHANNELS END */)) \
+  FUNC(( R16G16B16A16_SSCALED,            /* CHANNELS START */ ((Red, SScaled, 16))       ((Green, SScaled, 16))  ((Blue, SScaled, 16))   ((Alpha, SScaled, 16))    /* CHANNELS END */)) \
+  FUNC(( R32G32B32_FLOAT,                 /* CHANNELS START */ ((Red, Float, 32))         ((Green, Float, 32))    ((Blue, Float, 32))                               /* CHANNELS END */)) \
+  FUNC(( R32G32B32_UINT,                  /* CHANNELS START */ ((Red, UInt, 32))          ((Green, UInt, 32))     ((Blue, UInt, 32))                                /* CHANNELS END */)) \
+  FUNC(( R32G32B32_SINT,                  /* CHANNELS START */ ((Red, SInt, 32))          ((Green, SInt, 32))     ((Blue, SInt, 32))                                /* CHANNELS END */)) \
+  FUNC(( R64G64_FLOAT,                    /* CHANNELS START */ ((Red, Float, 64))         ((Green, Float, 64))                                                      /* CHANNELS END */)) \
+  FUNC(( R64G64_UINT,                     /* CHANNELS START */ ((Red, UInt, 64))          ((Green, UInt, 64))                                                       /* CHANNELS END */)) \
+  FUNC(( R64G64_SINT,                     /* CHANNELS START */ ((Red, SInt, 64))          ((Green, SInt, 64))                                                       /* CHANNELS END */)) \
+  FUNC(( R32G32B32A32_FLOAT,              /* CHANNELS START */ ((Red, Float, 32))         ((Green, Float, 32))    ((Blue, Float, 32))     ((Alpha, Float, 32))      /* CHANNELS END */)) \
+  FUNC(( R32G32B32A32_UINT,               /* CHANNELS START */ ((Red, UInt, 32))          ((Green, UInt, 32))     ((Blue, UInt, 32))      ((Alpha, UInt, 32))       /* CHANNELS END */)) \
+  FUNC(( R32G32B32A32_SINT,               /* CHANNELS START */ ((Red, UInt, 32))          ((Green, UInt, 32))     ((Blue, UInt, 32))      ((Alpha, UInt, 32))       /* CHANNELS END */)) \
+  FUNC(( R64G64B64_FLOAT,                 /* CHANNELS START */ ((Red, Float, 64))         ((Green, Float, 64))    ((Blue, Float, 64))                               /* CHANNELS END */)) \
+  FUNC(( R64G64B64_UINT,                  /* CHANNELS START */ ((Red, UInt, 64))          ((Green, UInt, 64))     ((Blue, UInt, 64))                                /* CHANNELS END */)) \
+  FUNC(( R64G64B64_SINT,                  /* CHANNELS START */ ((Red, SInt, 64))          ((Green, SInt, 64))     ((Blue, SInt, 64))                                /* CHANNELS END */)) \
+  FUNC(( R64G64B64A64_FLOAT,              /* CHANNELS START */ ((Red, Float, 64))         ((Green, Float, 64))    ((Blue, Float, 64))     ((Alpha, Float, 64))      /* CHANNELS END */)) \
+  FUNC(( R64G64B64A64_UINT,               /* CHANNELS START */ ((Red, UInt, 64))          ((Green, UInt, 64))     ((Blue, UInt, 64))      ((Alpha, UInt, 64))       /* CHANNELS END */)) \
+  FUNC(( R64G64B64A64_SINT,               /* CHANNELS START */ ((Red, SInt, 64))          ((Green, SInt, 64))     ((Blue, SInt, 64))      ((Alpha, SInt, 64))       /* CHANNELS END */)) \
+  FUNC(( D24_UNORM_S8_UINT,               /* CHANNELS START */ ((Depth, UNorm, 24))       ((Stencil, UInt, 8))                                                      /* CHANNELS END */)) \
+  FUNC(( D32_FLOAT_S8X24_UINT,            /* CHANNELS START */ ((Depth, Float, 32))       ((Stencil, UInt, 8))    ((Padding, Float, 24))                            /* CHANNELS END */)) \
+  FUNC(( D32_FLOAT,                       /* CHANNELS START */ ((Depth, Float, 32))                                                                                 /* CHANNELS END */))
+
+
 
 // Macros For: ENUM NAME
 #define GET_FORMAT_NAME_FROM_TUPLE(ENUM_TUPLE) BOOST_PP_TUPLE_ELEM(TUPLE_SIZE, TUPLE_ACCESS_ENUM, ENUM_TUPLE)
