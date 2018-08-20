0492275ff68d640e9511be01f8a5626df2a870eb
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-08-19 20:35:53 -0400

    add: base render system

---
+++ Source/Azura/RenderSystem/Inc/Generic/RenderSystem.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "Renderer.h"
+#include "Memory/Allocator.h"
+
+namespace Azura {
+namespace RenderSystem {
+  Renderer* CreateRenderer(Memory::Allocator& allocator);
+} // namespace RenderSystem
+} // namespace Azura
---
+++ Source/Azura/RenderSystem/Src/Vulkan/VkRenderSystem.cpp
@@ -0,0 +1,7 @@
+#include "Generic/RenderSystem.h"
+
+namespace Azura {
+Renderer* RenderSystem::CreateRenderer(Memory::Allocator& allocator) {
+	return nullptr;
+}
+} // namespace Azura
