4a247771820f3c5344ab1287636677f6d14a37a6
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-08-19 20:38:14 -0400

    fix: warnings in vkrendersystem

--- Source/Azura/RenderSystem/Src/Vulkan/VkRenderSystem.cpp
+++ Source/Azura/RenderSystem/Src/Vulkan/VkRenderSystem.cpp
@@ -1,7 +1,9 @@
 #include "Generic/RenderSystem.h"
+#include "Utils/Macros.h"
 
 namespace Azura {
 Renderer* RenderSystem::CreateRenderer(Memory::Allocator& allocator) {
-	return nullptr;
+  UNUSED(allocator);
+  return nullptr;
 }
 } // namespace Azura
