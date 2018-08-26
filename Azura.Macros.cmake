070dc53a2cc05d9b2bac1ce70fb79d75c563550b
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-08-26 12:28:38 -0400

    update: cmake format

--- Azura.Macros.cmake
+++ Azura.Macros.cmake
@@ -1,18 +1,18 @@
 macro(AzuraSilenceClangTidy TargetName)
-	set_target_properties(
-	    ${TargetName}
-	    PROPERTIES
-	         CXX_CLANG_TIDY ""
-	)
+  set_target_properties(${TargetName} PROPERTIES CXX_CLANG_TIDY "")
 endmacro(AzuraSilenceClangTidy)
 
+macro(AzuraAddBuildFlags TargetName)
+  if(CMAKE_BUILD_TYPE MATCHES DEBUG)
+    target_compile_definitions(${TargetName} PUBLIC BUILD_DEBUG=1)
+  elseif(CMAKE_BUILD_TYPE MATCHES RELEASE)
+    target_compile_definitions(${TargetName} PUBLIC BUILD_RELEASE=1)
+  endif()
+endmacro(AzuraAddBuildFlags)
 
 macro(AzuraAddUnitTest TargetName)
-	add_test(UnitTests ${TargetName})
+  add_test(UnitTests ${TargetName})
 
-	target_compile_definitions(${TargetName}
-	    PUBLIC
-	        BUILD_UNIT_TEST=1
-	)
+  target_compile_definitions(${TargetName} PUBLIC BUILD_UNIT_TEST=1)
 
 endmacro(AzuraAddUnitTest)
