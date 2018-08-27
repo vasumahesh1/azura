6319b2d74f65cb38706db6e756b6631d227ec5f6
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-08-26 22:58:21 -0400

    add: yaml submodule

--- Azura.Macros.cmake
+++ Azura.Macros.cmake
@@ -2,6 +2,33 @@ macro(AzuraSilenceClangTidy TargetName)
   set_target_properties(${TargetName} PROPERTIES CXX_CLANG_TIDY "")
 endmacro(AzuraSilenceClangTidy)
 
+macro(AzuraSilenceEnumGuard TargetName)
+  set_target_properties(${TargetName}
+                        PROPERTIES COMPILE_FLAGS "${CMAKE_CXX_FLAGS} /wd4061")
+endmacro(AzuraSilenceEnumGuard)
+
+macro(AzuraDisableWarningsAsErrors)
+  if(MSVC)
+    string(REPLACE " /WX"
+                   ""
+                   CMAKE_CXX_FLAGS
+                   "${CMAKE_CXX_FLAGS}")
+  elseif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+    string(REPLACE " -Werror"
+                   ""
+                   CMAKE_CXX_FLAGS
+                   "${CMAKE_CXX_FLAGS}")
+  endif()
+endmacro(AzuraDisableWarningsAsErrors)
+
+macro(AzuraEnableWarningsAsErrors)
+  if(MSVC)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
+  elseif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
+  endif()
+endmacro(AzuraEnableWarningsAsErrors)
+
 macro(AzuraAddBuildFlags TargetName)
   if(CMAKE_BUILD_TYPE MATCHES DEBUG)
     target_compile_definitions(${TargetName} PUBLIC BUILD_DEBUG=1)
