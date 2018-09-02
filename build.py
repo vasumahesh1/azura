25ea5d77d97b52c6221b699c5cf00dbbdcaaec0a
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-09-02 01:59:29 -0400

    fix Issue #5: updated for 32 & 64bit Windows GLFW builds for Vulkan Render System

--- build.py
+++ build.py
@@ -22,7 +22,7 @@ currentBuildArch = "64"
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc:  # Python >2.5
+    except OSError as exc:
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else:
