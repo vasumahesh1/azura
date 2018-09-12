46935918a526a12addcd68292b4970920800a4e5
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-09-11 22:35:31 -0400

    fix: set boost library dirs based on arch and os

--- Source/Boost.Import.cmake
+++ Source/Boost.Import.cmake
@@ -4,10 +4,17 @@ if ("${BUILD_PLATFORM}" STREQUAL "Windows")
         CACHE STRING ""
         FORCE)
   endif()
+
+  set(LIB64_BOOST "lib64-msvc-14.1")
+  set(LIB32_BOOST "lib32-msvc-14.1")
+
+  if ("${BUILD_ARCH}" STREQUAL "64")
+    set(BOOST_LIBRARYDIR "${BOOST_ROOT}/${LIB64_BOOST}/" CACHE STRING "" FORCE)
+  elseif("${BUILD_ARCH}" STREQUAL "32")
+    set(BOOST_LIBRARYDIR "${BOOST_ROOT}/${LIB32_BOOST}/" CACHE STRING "" FORCE)
+  endif()
 endif()
 
-
-set(BOOST_ROOT ${BOOST_ROOT} CACHE STRING "" FORCE)
 set(Boost_USE_STATIC_LIBS ON)
 find_package(Boost ${BOOST_VERSION} EXACT REQUIRED COMPONENTS log filesystem system)
 
